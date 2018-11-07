365f07af8ee73fa1fbdcb7f09ded6b933543ab6f
cisglee <cisglee@gmail.com>
2018-11-07 20:25:35 +0100

    Update waterTest.py

--- tests/waterTest.py
+++ tests/waterTest.py
@@ -4,15 +4,15 @@ import checkpy.assertlib as assertlib
 
 @t.test(0)
 def exactWater0(test):
-	test.test = lambda : assertlib.numberOnLine(0, lib.getLine(lib.outputOf(_fileName, [0]), 0))
-	test.description = lambda : "0 minutes under the shower equals 0 water bottles"
+	test.test = lambda : assertlib.numberOnLine(0, lib.getLine(lib.outputOf(_fileName, stdinArgs=[0]), 0))
+	test.description = lambda : "0 minuten douchen staat gelijk aan 0 water flesjes"
 
 @t.test(1)
 def exactWater10(test):
-	test.test = lambda : assertlib.numberOnLine(120, lib.getLine(lib.outputOf(_fileName, [10]), 0))
-	test.description = lambda : "10 minutes under the shower equals 120 water bottles"
+	test.test = lambda : assertlib.numberOnLine(120, lib.getLine(lib.outputOf(_fileName, stdinArgs=[10]), 0))
+	test.description = lambda : "10 minuten douchen staat gelijk aan 120 water flesjes"
 
 @t.test(2)
 def exactWater327(test):
-	test.test = lambda : assertlib.numberOnLine(3924, lib.getLine(lib.outputOf(_fileName, [327]), 0))
-	test.description = lambda : "327 minutes under the shower equals 3924 water bottles"
+	test.test = lambda : assertlib.numberOnLine(3924, lib.getLine(lib.outputOf(_fileName, stdinArgs=[327]), 0))
+	test.description = lambda : "327 minuten douchen staat gelijk aan 3924 water flesjes"
