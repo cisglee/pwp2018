c8b2d27e6b94566a4a06bc8944cac3104c6f94c4
cisglee <cisglee@gmail.com>
2018-11-12 13:38:22 +0100

    Update plotTest.py

--- tests/module1/plotTest.py
+++ tests/module1/plotTest.py
@@ -9,6 +9,6 @@ def showsGraph(test):
 
 @t.test(1)
 def exactZeroPoint(test):
-	test.test = lambda : assertlib.numberOnLine(0.37, lib.getLine(lib.outputOf(_fileName), stdinArgs=[0]), deviation = 0.01)\
-				     and assertlib.numberOnLine(0.69, lib.getLine(lib.outputOf(_fileName), stdinArgs=[0]), deviation = 0.01)
+	test.test = lambda : assertlib.numberOnLine(0.37, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)\
+				     and assertlib.numberOnLine(0.69, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)
 	test.description = lambda : "prints the intersects of the axes (x = 0.37 and y = 0.69)"
