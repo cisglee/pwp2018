c83cd6e117be8e13e3bb2b40d699d2897526e567
cisglee <cisglee@gmail.com>
2018-11-22 12:29:40 +0100

    Update countmatchesTest.py

--- tests/module2/countmatchesTest.py
+++ tests/module2/countmatchesTest.py
@@ -5,13 +5,13 @@ import checkpy.assertlib as assertlib
 @t.test(0)
 def has_count_exact_matches(test):
     test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "count_exact_matches")
-    test.description = lambda : "definieert de functie count_exact_matches"
+    test.description = lambda : "defines the function count_exact_matches"
 
 @t.passed(has_count_exact_matches)
 @t.test(1)
 def is_int_count_exact_matches(test):
     test.test = lambda : assertlib.sameType(lib.getFunction("count_exact_matches", _fileName)("a", "a"), 0)
-    test.description = lambda : "count_exact_matches geeft een integer terug"
+    test.description = lambda : "count_exact_matches returns the right integer"
 
 @t.passed(is_int_count_exact_matches)
 @t.test(2)
@@ -19,7 +19,7 @@ def count_exact_matches_2(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("atgacatgcacaagtatgcat", "atgc"), 2)
-    test.description = lambda : "count_exact_matches werkt voor invoer 'atgacatgcacaagtatgcat', 'atgc'"
+    test.description = lambda : "count_exact_matches works for input 'atgacatgcacaagtatgcat', 'atgc'"
 
 @t.passed(is_int_count_exact_matches)
 @t.test(3)
@@ -27,7 +27,7 @@ def count_exact_matches_8(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("atgacatgcacaagtatgcat", "a"),  8)
-    test.description = lambda : "count_exact_matches werkt voor invoer 'atgacatgcacaagtatgcat', 'a'"
+    test.description = lambda : "count_exact_matches works for input 'atgacatgcacaagtatgcat', 'a'"
 
 @t.passed(is_int_count_exact_matches)
 @t.test(4)
@@ -35,7 +35,7 @@ def count_exact_matches_0a(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("atgacatgcacaagtatgcat", "b"), 0)
-    test.description = lambda : "count_exact_matches werkt voor invoer 'atgacatgcacaagtatgcat', 'b'"
+    test.description = lambda : "count_exact_matches works for input 'atgacatgcacaagtatgcat', 'b'"
 
 @t.passed(is_int_count_exact_matches)
 @t.test(5)
@@ -43,4 +43,4 @@ def count_exact_matches_0b(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("", "atgc"), 0)
-    test.description = lambda : "count_exact_matches werkt voor invoer '', 'atgc'"
+    test.description = lambda : "count_exact_matches works for input '', 'atgc'"
