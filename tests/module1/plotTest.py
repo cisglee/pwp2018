0f5ff6f97d96ad1aa2ae1e298bc68d4902ec36a7
cisglee <cisglee@gmail.com>
2018-12-02 16:27:51 +0100

    Update plotTest.py

--- tests/module1/plotTest.py
+++ tests/module1/plotTest.py
@@ -6,9 +6,3 @@ import checkpy.assertlib as assertlib
 def showsGraph(test):
 	test.test = lambda : assertlib.fileContainsFunctionCalls(_fileName, "savefig")
 	test.description = lambda : "stores a graph"
-
-@t.test(1)
-def exactZeroPoint(test):
-	test.test = lambda : assertlib.numberOnLine(0.37, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)\
-				     and assertlib.numberOnLine(0.69, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)
-	test.description = lambda : "prints the intersects of the axes (x = 0.37 and y = 0.69)"
