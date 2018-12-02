fcca00a23484757652ef62888bddbdb8961ce5a4
cisglee <cisglee@gmail.com>
2018-12-02 18:04:45 +0100

    Update initialsTest.py

--- tests/module2/initialsTest.py
+++ tests/module2/initialsTest.py
@@ -5,28 +5,28 @@ import checkpy.assertlib as assertlib
 @t.test(0)
 def containsRequiredFunctionDefinitions(test):
 	test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "initials")
-	test.description = lambda : "definieert de functie initials()"
+	test.description = lambda : "defines the function initials()"
 
 @t.passed(containsRequiredFunctionDefinitions)
 @t.test(10)
 def correctInitials1(test):
 	test.test = lambda : assertlib.exact(lib.getFunction("initials", _fileName)("Johann Friedrich Miescher").upper(), "JFM")
-	test.description = lambda : "initials() geeft de correcte initialen van \"Johann Friedrich Miescher\""
+	test.description = lambda : "initials() returns the correct initials for \"Johann Friedrich Miescher\""
 
 @t.passed(containsRequiredFunctionDefinitions)
 @t.test(11)
 def correctInitials2(test):
 	test.test = lambda : assertlib.exact(lib.getFunction("initials", _fileName)("Phoebus Levene").upper(), "PL")
-	test.description = lambda : "initials() geeft de correcte initialen van \"Phoebus Levene\""
+	test.description = lambda : "initials() returns the correct initials for \"Phoebus Levene\""
 
 @t.passed(containsRequiredFunctionDefinitions)
 @t.test(12)
 def correctInitials3(test):
 	test.test = lambda : assertlib.exact(lib.getFunction("initials", _fileName)("Martha Chase").upper(), "MC")
-	test.description = lambda : "initials() geeft de correcte initialen van \"Martha Chase\""
+	test.description = lambda : "initials() returns the correct initials for \"Martha Chase\""
 
 @t.passed(containsRequiredFunctionDefinitions)
 @t.test(20)
 def almostWrongInput(test):
 	test.test = lambda : assertlib.exact(lib.getFunction("initials", _fileName)("John  Double  Spaces").upper(), "JDS")
-	test.description = lambda : "initials() geeft de correcte initialen van \"John  Double  Spaces\""
+	test.description = lambda : "initials() returns the correct initials for \"John  Double  Spaces\""
