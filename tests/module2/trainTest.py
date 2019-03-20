9a1be4c0a37913dc82e7fe048b64c538d1f3c3b4
cisglee <cisglee@gmail.com>
2019-03-20 09:02:13 +0100

    Update trainTest.py

--- tests/module2/trainTest.py
+++ tests/module2/trainTest.py
@@ -8,23 +8,3 @@ def containsOneQuestion(test):
 	test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 0), "minutes") and assertlib.contains(
 		lib.getLine(lib.outputOf(_fileName), 0), "?")
 	test.description = lambda : "Could not ascertain whether you asked how many minutes it would take to get to the station"
-
-
-@t.test(1)
-def containsTwoQuestions(test):
-	test.test = lambda: assertlib.contains(lib.getLine(lib.outputOf(_fileName), 0), "minutes") and assertlib.contains(
-		lib.getLine(lib.outputOf(_fileName), 0), "?")
-	test.description = lambda : "Could not ascertain whether you asked how many minutes it would take to get from the station to the destination"
-
-# TODO: Check how arguments for separate questions can be provided
-@t.test(2)
-def containsPeak(test):
-	def containsCheck():
-		if assertlib.contains(lib.getLine(lib.outputOf(_fileName), [18, 12]).lower(), "peak"):  # Possibly provides all arguments at once, instead of waiting for the second question
-			return True
-		elif assertlib.contains(lib.getLine(lib.outputOf(_fileName), [18, 12]).lower(), "rush"):
-			return True
-		else:
-			return False
-	test.test = containsCheck
-	test.description = lambda : "Could not find any mention of off-peak or peak hours"
