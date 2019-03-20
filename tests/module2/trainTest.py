b574c448eeac96727aa7d8334baefc8cc2bdb181
cisglee <cisglee@gmail.com>
2019-03-20 09:21:10 +0100

    Update trainTest.py

--- tests/module2/trainTest.py
+++ tests/module2/trainTest.py
@@ -5,5 +5,7 @@ import checkpy.assertlib as assertlib
 # TODO: Add test to check for correct times, using Excel workbook values
 @t.test(0)
 def containsTrain1(test):
-	test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 0), "minutes")
-	test.description = lambda : "Could not ascertain whether you asked how many minutes it would take to get to the station"
+    print(lib.getLine(lib.outputOf(_fileName), 0).lower())
+    test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 0).lower(), "minutes") and assertlib.contains(
+		lib.getLine(lib.outputOf(_fileName), 0), "?")
+    test.description = lambda : "Could not ascertain whether you asked how many minutes it would take to get to the station"
