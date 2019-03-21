400daf77b61f9f13c076fa88ad32aa2c2eca8a94
cisglee <cisglee@gmail.com>
2019-03-21 16:25:54 +0100

    Update trainTest.py

--- tests/module2/trainTest.py
+++ tests/module2/trainTest.py
@@ -1,10 +1,42 @@
 import checkpy.tests as t
 import checkpy.lib as lib
-import checkpy.assertlib as assertlib
+import re
 
-# TODO: Add test to check for correct times, using Excel workbook values
+
 @t.test(0)
+def containsTrain0(test):
+    def containsCheck():
+        output = lib.outputOf(test.fileName, stdinArgs=[22, 15])
+        time = re.search("\d{1,2}:\d{2}", output)
+        if time is None:
+            return False
+        else:
+            return True
+
+    test.test = containsCheck
+    test.description = lambda: "Checking whether you provided the time"
+
+@t.test(1)
 def containsTrain1(test):
-    test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 0).lower(), "minutes") and assertlib.contains(
-		lib.getLine(lib.outputOf(_fileName), 0), "?")
-    test.description = lambda : "Could not ascertain whether you asked how many minutes it would take to get to the station\n{}".format(lib.getLine(lib.outputOf(_fileName), 0).lower())
+    def containsCheck():
+        output = lib.outputOf(test.fileName, stdinArgs=[22, 15])
+        time = re.search("(10a|11a|13a|14a|15a|15b)", output)
+        if time is None:
+            return False
+        else:
+            return True
+
+    test.test = containsCheck
+    test.description = lambda: "Checking whether you provide the track number"
+
+@t.test(2)
+def containsPeak18_22(test):
+    def containsCheck():
+        output = lib.outputOf(test.fileName, stdinArgs=[18, 12])
+        if "peak" in output or "rush" in output:
+            return True
+        else:
+            return False
+
+    test.test = containsCheck
+    test.description = lambda: "Checking whether you mentioned peak hours"
